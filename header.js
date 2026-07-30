fbf945e29243e00ace96c0183f0a81cc30cf5569
BUAYA <pic_gth@clickgroup.id>
2026-07-30 09:22:04 +0700

    Update header.js

--- header.js
+++ header.js
@@ -2,7 +2,7 @@
     const css = `
         @media (min-width: 992px) {
             .c-qiOHF {
-                background: url("https://plcl.me/images/YcYh7.jpg") center center / cover no-repeat !important;
+                background: url("http://plcl.me/images/bysNR.jpg") center center / cover no-repeat !important;
             }
 
             .c-qiOHF::before {
