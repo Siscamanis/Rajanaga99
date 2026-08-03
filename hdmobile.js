58b98d926585ebcca7fd7bdd2a7a723fd3a2e29f
BUAYA <pic_gth@clickgroup.id>
2026-08-03 09:58:33 +0700

    Update hdmobile.js

--- hdmobile.js
+++ hdmobile.js
@@ -3,7 +3,7 @@
         /* MOBILE */
         @media (max-width: 991px) {
             .c-ptpKx {
-                background: url("http://plcl.me/images/sZVqh.jpg") center / cover no-repeat !important;
+                background: url("http://plcl.me/images/UdCzt.jpg") center / cover no-repeat !important;
             }
 
             .c-ptpKx img[alt="header-bg"] {
@@ -14,7 +14,7 @@
         /* DESKTOP */
         @media (min-width: 992px) {
             .c-qiOHF {
-                background: url("http://plcl.me/images/zwLyK.jpg") center center / cover no-repeat !important;
+                background: url("http://plcl.me/images/JyJMb.jpg") center center / cover no-repeat !important;
             }
 
             .c-qiOHF::before {
